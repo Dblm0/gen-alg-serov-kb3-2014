2af7245caaa1beec828b16c895c6f27378c2a442
DmitrievDmitriyMsk@gmail.com <DmitrievDmitriyMsk@gmail.com@bc9dfece-6a7c-0f9d-da40-ac0d80cf3071>
2014-11-16 17:34:59 +0000

    Алгоритм генерации пар вынесен в отдельный файл

--- Test.m
+++ Test.m
@@ -58,35 +58,7 @@ for i = 1 : Xn/2
     indexes(pos) = [];
 end
 % формирование пар родителей
-Pairs = zeros(Xn/2,2);
-indexes = 1 : Xn/2;
-for i = 1 : Xn/4
-    for j = 1 : 2
-        r = indexes(randi([1 size(indexes,2)]));
-        pos = indexes == r;
-        Pairs(i,j) = r;
-        indexes(pos) = [];
-    end
-end
-% формирование второго набора пар, отличного от первого
-
-indexes = 1 : Xn/2;
-for i = Xn/4 + 1 : Xn/2
-    r = indexes(randi([1 size(indexes,2)]));
-    pos = indexes == r;
-    Pairs(i,1) = r;
-    indexes(pos) = [];
-    cs1 = 1;
-    cs2 = 1;
-    while cs1 || cs2
-        r = indexes(randi([1 size(indexes,2)]));
-        pos = indexes == r;
-        Pairs(i,2) = r;
-        cs1 =  ismember(Pairs(i,:),Pairs(1:Xn/4,:),'rows');
-        cs2 = ismember(fliplr(Pairs(i,:)),Pairs(1:Xn/4,:),'rows');
-    end
-    indexes(pos) = [];  
-end
+Pairs = GeneratePairs(size(Parents,2));
 
 clear b j i pos r cs1 cs2;
 
