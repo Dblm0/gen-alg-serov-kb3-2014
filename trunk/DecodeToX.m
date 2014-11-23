49b6c11ea262bc354ef631db3aea717e2f1c73b6
DmitrievDmitriyMsk@gmail.com <DmitrievDmitriyMsk@gmail.com@bc9dfece-6a7c-0f9d-da40-ac0d80cf3071>
2014-11-23 13:44:54 +0000

    Поправочка

--- trunk/DecodeToX.m
+++ trunk/DecodeToX.m
@@ -6,8 +6,7 @@ G = reshape(bits,K,2)';
 B = zeros(2,K);
 X = zeros(2,L);
 for i = 1:2
-    B(i,:) = Translate(G(i,:),'Bin');
-    X(i,:) = BinGDecode(B(i,:), Xrange );
+    X(i,:) = BinGDecode(G(i,:), Xrange );
 end
 end
 
