6abe2124f8c19353656e78994d5a6de9b98a49f2
DmitrievDmitriyMsk@gmail.com <DmitrievDmitriyMsk@gmail.com@bc9dfece-6a7c-0f9d-da40-ac0d80cf3071>
2014-12-02 22:01:56 +0000

    Оптимизирован алгоритм GetDistances вызывается единожды

--- Комивояжер/GetDistMatr.m
+++ Комивояжер/GetDistMatr.m
@@ -1,10 +1,10 @@
 function Matr = GetDistMatr(Points)
 N = size(Points,1);
 Matr = zeros(N);
+D = GetDistances(Points);
 for i = 1 : N - 1
-    D = GetDistances(Points , i);
-    Matr(i , i : N) = D';
-    Matr(i : N , i) = D;
+    Matr(i , i : N) = D(1 : N - i + 1)';
+    Matr(i : N , i) = D(1 : N - i + 1);
 end
 end
 
