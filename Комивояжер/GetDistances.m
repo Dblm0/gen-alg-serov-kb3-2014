a52591e0a791f9537ec948850864f17197d4c91a
DmitrievDmitriyMsk@gmail.com <DmitrievDmitriyMsk@gmail.com@bc9dfece-6a7c-0f9d-da40-ac0d80cf3071>
2014-12-02 22:11:11 +0000

    Переписан алгоритм

--- Комивояжер/GetDistances.m
+++ Комивояжер/GetDistances.m
@@ -1,9 +1,7 @@
-function Distances = GetDistances(Points,Nstart)
+function Distances = GetDistances(Points)
 N = size(Points,1);
-A = Points(Nstart,:);
-K = N - Nstart + 1;
-Distances = zeros(K , 1);
-Distances(1) = 0;
-i = Nstart + 1 : N;
-Distances(2 : K) = sqrt( (A(1)-Points(i,1)).^2 + (A(2)-Points(i,2)).^2 );
+A = Points(1,:);
+Distances = zeros(N , 1);
+i = 2 : N;
+Distances(i) = sqrt( (A(1)-Points(i,1)).^2 + (A(2)-Points(i,2)).^2 );
 
