fee76f3960f843c7e12f1c42e8e00dcf8a2b1ea4
DmitrievDmitriyMsk@gmail.com <DmitrievDmitriyMsk@gmail.com@bc9dfece-6a7c-0f9d-da40-ac0d80cf3071>
2014-12-21 19:56:01 +0000

    Упрощен алгоритм формирования пар родителей

--- GeneratePairs.m
+++ GeneratePairs.m
@@ -10,25 +10,15 @@ for i = 1 : Count/2
         indexes(pos) = [];
     end
 end
-% формирование второго набора пар, отличного от первого
-
+% формирование второго набора пар
 indexes = 1 : Count;
 for i = Count/2 + 1 : Count
-    r = indexes(randi([1 size(indexes,2)]));
-    pos = indexes == r;
-    Pairs(i,1) = r;
-    indexes(pos) = [];
-    cs1 = 1;
-    cs2 = 1;
-    while cs1 || cs2
+    for j = 1 : 2
         r = indexes(randi([1 size(indexes,2)]));
         pos = indexes == r;
-        Pairs(i,2) = r;
-        cs1 =  ismember(Pairs(i,:),Pairs(1:Count/2,:),'rows');
-        cs2 = ismember(fliplr(Pairs(i,:)),Pairs(1:Count/2,:),'rows');
+        Pairs(i,j) = r;
+        indexes(pos) = [];
     end
-    indexes(pos) = [];  
 end
-
 end
 
